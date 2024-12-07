dec8c5474b6c43fe3c0038380e651ffe215bca0d
OwenWaldron <owen.r.waldron@gmail.com>
2024-12-07 16:43:04 -0500

    Added react router

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='w-full'>
+            Hello, header!
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/pages/projects/ProjectPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectPage = () => {
+    return <h1>Hello, Projects!</h1>
+}
+
+export default ProjectPage;
