83cb669a3c52433dc366495bd751b20f1b35cc9a
OwenWaldron <owen.r.waldron@gmail.com>
2024-12-07 17:45:18 -0500

    Oops, added header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import Owen from '../../assets/images/owensmall.png';
+import styles from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='w-full'>
-            Hello, header!
+        <header className='flex min-h-36 mb-12 bg-header-gray border-b-4 border-header-accent-gray text-white martian-mono'>
+            <div className='flex justify-between mx-auto lg:min-w-[1024px] min-w-full pb-4'>
+                <Link to='/' className='flex flex-col items-center mt-auto'>
+                    <img className='max-w-16 pb-2' src={Owen} alt='' />
+                    <h1 className='text-white'>Owen Waldron</h1>
+                </Link>
+                <div className='flex gap-8 mt-auto'>
+                    <Link className={styles['hover-underline']} to={'/projects'} >
+                        Projects
+                    </Link>
+                    <Link className={styles['hover-underline']} to={'/about'} >
+                        About
+                    </Link>
+                </div>
+            </div>
         </header>
     )
 }
